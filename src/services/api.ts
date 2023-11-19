06bd64e2a392788fce849140a7cd20b11752f984
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-11-19 17:58:49 -0300

    atualizando redux

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { CardapioItem, Restaurante } from '../pages/Home'
+import { Restaurante } from '../pages/Home'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
